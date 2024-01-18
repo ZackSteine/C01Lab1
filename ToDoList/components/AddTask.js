d9b832465eb493cf675c3860d174318d097683df
ZackSteine <zack.steine@mail.utoronto.ca>
2024-01-18 16:37:53 -0500

    Removed style comments

--- ToDoList/components/AddTask.js
+++ ToDoList/components/AddTask.js
@@ -27,17 +27,4 @@ const AddTask = ({ onAddTask }) => {
     );
 };
 
-/* const styles = StyleSheet.create({
-    addTodoForm: {
-      margin: 10,
-    },
-    input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginBottom: 10,
-      paddingHorizontal: 10,
-    },
-}); */
-
 export default AddTask;
--- ToDoList/components/ToDoList.js
+++ ToDoList/components/ToDoList.js
@@ -34,20 +34,4 @@ ToDoList.defaultProps = {
     initialValues: []
 };
 
-/* const styles = StyleSheet.create({
-    todoListContainer: {
-      margin: 10,
-    },
-    todoItem: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: 10,
-      marginVertical: 5,
-      borderColor: 'gray',
-      borderWidth: 1,
-      borderRadius: 5,
-    },
-}); */
-
 export default ToDoList;
